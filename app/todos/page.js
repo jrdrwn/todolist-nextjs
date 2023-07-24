bdb7856d3067761a3a0f5919a1942f4b5996d294
rahmat <rahmatgamming1@gmail.com>
2023-07-24 18:19:21 +0800

    ahu

--- app/todos/page.js
+++ app/todos/page.js
@@ -1,6 +1,7 @@
 import AddTodo from "../components/AddTodo";
 import Title from "../components/Title";
 import TodoList from "../components/TodoList";
+import prisma from "@/app/libs/prismadb";
 
 require("dotenv").config();
 
@@ -13,16 +14,34 @@ const apiUrl =
   process.env.NEXT_LOCAL_API_URL || process.env.NEXT_PUBLIC_API_URL;
 
 async function getTodosData() {
-  const res = await fetch(apiUrl + "/api/posts", {
-    cache: "no-store",
-  });
-  if (!res.ok) {
-    throw new Error(res.status);
+  // const res = await fetch(apiUrl + "/api/posts", {
+  //   cache: "no-store",
+  // });
+  //
+  // const res = await prisma.post.findMany();
+  // if (!res.ok) {
+  //   throw new Error(res.status);
+  // }
+  // const todos = await res.json();
+  // // Sort the todos based on the createdAt date in descending order
+  // todos.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+  // return todos;
+  try {
+    const todos = await prisma.post.findMany();
+    // Assuming `findMany` directly returns the todos array
+    // If not, adjust accordingly based on the response structure.
+    // For example, if it's { data: todos }, you'd use `const todos = res.data;`.
+
+    // Sort the todos based on the createdAt date in descending order
+    const sortedTodos = todos.sort((a, b) => b.createdAt - a.createdAt);
+
+    // Use sortedTodos as needed.
+    return sortedTodos;
+  } catch (error) {
+    // Handle the error here, for example, log the error or return an empty array.
+    console.error("Error fetching todos:", error);
+    return [];
   }
-  const todos = await res.json();
-  // Sort the todos based on the createdAt date in descending order
-  todos.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-  return todos;
 }
 
 const page = async () => {
