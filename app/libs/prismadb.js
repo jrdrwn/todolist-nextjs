4cb62be1fb3bb7c981151628adeb0599ba072f26
rahmat <rahmatgamming1@gmail.com>
2023-07-24 16:33:02 +0800

    fix error

--- app/libs/prismadb.js
+++ app/libs/prismadb.js
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
+require("dotenv").config();
+
 const client = globalThis.prisma || new PrismaClient();
 if (process.env.NODE_ENV === "development") globalThis.prisma = client;
 
